cd8f75af4a5af93932b3b789c6472cc251f7eef1
ksahib <kazi.sahib@northsouth.edu>
2026-04-21 03:55:20 +0600

    unwarping method

--- featurenet/models/blocks.py
+++ featurenet/models/blocks.py
@@ -2,15 +2,22 @@ import torch
 import torch.nn as nn
 
 
+def _resolve_group_count(num_channels: int, max_groups: int = 32) -> int:
+    for groups in range(min(max_groups, num_channels), 0, -1):
+        if num_channels % groups == 0:
+            return groups
+    return 1
+
+
 class ConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1):
         super().__init__()
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
-        self.bn = nn.BatchNorm2d(out_channels)
+        self.norm = nn.GroupNorm(_resolve_group_count(out_channels), out_channels)
         self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
         x = self.conv(x)
-        x = self.bn(x)
+        x = self.norm(x)
         x = self.relu(x)
         return x
